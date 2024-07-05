c8631ed5be0d196cc0c1d6538f3aa7477a164b3a
ItzTas <talessabini2@gmail.com>
2024-07-04 23:01:00 -0300

    publishJSON

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	connstr := os.Getenv("CONN_STR")
+
+	conn, err := amqp.Dial(connstr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Sucessfully connected")
+
+	connChan, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pubsub.PublishJSON(connChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+
+	fmt.Println("Shutting down program")
 }
